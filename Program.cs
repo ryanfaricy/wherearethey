3c1c6e7f0e025f648b638295b77c2f87eb7daf6d
Ryan Faricy <18222041+ryanfaricy@users.noreply.github.com>
2026-01-12 11:00:31 -0600

    Change `LocationService` from scoped to singleton in DI configuration to make heatmap updates 'live'

--- Program.cs
+++ Program.cs
@@ -75,7 +75,7 @@ builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString ?? "Host=localhost;Database=wherearethey;Username=postgres;Password=postgres"));
 
 // Add application services
-builder.Services.AddScoped<LocationService>();
+builder.Services.AddSingleton<LocationService>();
 builder.Services.AddScoped<AlertService>();
 builder.Services.AddScoped<DonationService>();
 builder.Services.AddScoped<AppThemeService>();
